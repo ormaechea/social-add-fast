23c7a5d2d3720a87ddecd4436def5dc6e031f646
Andres Castillo <castilloandres17@gmail.com>
2015-08-19 21:43:08 -0700

    Add networks Create, Read and Update

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -1,21 +1,49 @@
 class NetworksController < ApplicationController
+
+
   def show
   end
 
+  def new
+    @user = User.find(params[:user_id])
+    @network = Network.new
+    @network.user_id = @user.id
+  end
+
+  def index
+   @user = User.find(params[:user_id])
+   @networks = @user.networks
+  end
+
   def create
-    @network = Network.create(network_params)
-    redirect_to ''
+    @network = Network.new(network_params)
+    @network.user_id = params[:user_id]
+    @network.save
+    redirect_to action:"index", user_id: params[:user_id]
   end
 
-  def update
-  end
+ def edit
+  @user = User.find(params[:user_id])
+  @network = Network.find(params[:id])
+ end
 
+ def update
+  @network = Network.find(params[:id])
+     if @network.update(network_params)
+     redirect_to action:"index", user_id: params[:user_id]
+    else
+      render 'edit'
+    end
+ end
   def destroy
+     @network = Network.find(params[:id])
+     @network.destroy
+     render 'index'
   end
 
   private
   def network_params
-    params.require(:network).permit(:name, :username, user_id: params[:user_id])
+    params.require(:network).permit(:name, :username)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818185956) do
+ActiveRecord::Schema.define(version: 20150819192822) do
+
+  create_table "networks", force: :cascade do |t|
+    t.string   "name"
+    t.string   "username"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
